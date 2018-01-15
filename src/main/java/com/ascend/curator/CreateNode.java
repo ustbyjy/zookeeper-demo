c242a07df516a37d305a8180f8dca327c4ea8715
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-01-15 16:12:57 +0800

    连续操作丢失事件

--- src/main/java/com/ascend/curator/CreateNode.java
+++ src/main/java/com/ascend/curator/CreateNode.java
@@ -11,6 +11,9 @@ import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * CacheNode会缓存本地数据，若连续对节点进行操作，通知事件会比较新旧节点的数据，有可能会丢失事件，因此连续操作的中间可以休眠一定的时间避免丢失事件。
+ */
 public class CreateNode {
     private static Logger logger = LoggerFactory.getLogger(CreateNode.class);
 
@@ -28,20 +31,31 @@ public class CreateNode {
         // 建立连接
         client.start();
 
-        String path = client.create()
-                .creatingParentsIfNeeded() // 如果没有父节点，则创建父节点
-                .withMode(CreateMode.PERSISTENT)
-                .forPath("/jike/1/11/111", "123".getBytes());
-
-        logger.info("return path：" + path);
-
         final NodeCache nodeCache = new NodeCache(client, "/jike/1/11/111");
         nodeCache.start();
         nodeCache.getListenable().addListener(new NodeCacheListener() {
             public void nodeChanged() throws Exception {
-                logger.info("node created");
+                if (nodeCache.getCurrentData() != null) {
+                    logger.info("node changed，data={}", new String(nodeCache.getCurrentData().getData()));
+                } else {
+                    logger.info("node deleted");
+                }
             }
         });
+
+        client.create()
+                .creatingParentsIfNeeded() // 如果没有父节点，则创建父节点
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath("/jike/1/11/111", "init".getBytes());
+
+//        Thread.sleep(100);
+        client.setData().forPath("/jike/1/11/111", "update1".getBytes());
+
+//        Thread.sleep(100);
+        client.setData().forPath("/jike/1/11/111", "update2".getBytes());
+
+//        Thread.sleep(100);
+        client.delete().forPath("/jike/1/11/111");
 
         System.in.read();
     }
